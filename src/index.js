57088edba9fcae71dedfffaa1bc64262444cbb01
Mahitha Mahi <sandhani.sk45@gmail.com>
2024-01-13 20:10:17 +0530

    setup gh-pages

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import { GlobalProvider } from "./context/global.context";
 
  
 async function enableMocking() {
+  debugger
   if (process.env.NODE_ENV !== 'development') {
+    debugger
     return
   }
  
@@ -27,14 +29,4 @@ enableMocking().then(() => {
   );
 })
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <GlobalProvider>
-//     <App />
-//   </GlobalProvider>
-// );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
